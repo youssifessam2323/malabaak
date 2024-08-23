7466698d11bb22ce866ecc2c90b5e5cefbec5ec7
Youssif Essam <youssifessambakry@gmail.com>
2024-08-23 18:31:12 +0300

    init application

---
+++ src/main/java/io/joework/malabaabapi/MalabaabApiApplication.java
@@ -0,0 +1,13 @@
+package io.joework.malabaabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalabaabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalabaabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/joework/malabaabapi/MalabaabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joework.malabaabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalabaabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
